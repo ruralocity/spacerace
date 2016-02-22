e596b4ea9bac2538b5e81212263049cd26108ed6
Aaron Sumner <asumner@mac.com>
2016-02-22 05:43:16 -0600

    Add end scene with final score

--- main.rb
+++ main.rb
@@ -14,16 +14,16 @@ class SpaceRace < Gosu::Window
     @opponents = []
     @score = 0
     @score_display = Gosu::Font.new(28)
+    @scene = :game
   end
 
   def draw
-    @background.draw(0, 0, 0)
-    @racer.draw
-    @opponents.each do |opponent|
-      opponent.draw
+    case @scene
+    when :game
+      draw_game
+    when :end
+      draw_end
     end
-
-    @score_display.draw(@score, 160, 20, 1, 1, 1, Gosu::Color::AQUA)
   end
 
   def update
@@ -37,9 +37,14 @@ class SpaceRace < Gosu::Window
 
     @opponents.each do |opponent|
       opponent.move
+
       if (opponent.y + opponent.radius > @racer.y + @racer.radius) && !opponent.passed
         @score += 1
         opponent.passed = true
+      end
+
+      if collision?(opponent, @racer)
+        @scene = :end
       end
     end
 
@@ -47,6 +52,33 @@ class SpaceRace < Gosu::Window
       if opponent.y > HEIGHT + opponent.radius
         @opponents.delete opponent
       end
+    end
+  end
+
+  private
+
+  def draw_game
+    @background.draw(0, 0, 0)
+    @racer.draw
+    @opponents.each do |opponent|
+      opponent.draw
+    end
+
+    @score_display.draw(@score, 160, 20, 1, 1, 1, Gosu::Color::AQUA)
+  end
+
+  def draw_end
+    @background.draw(0, 0, 0)
+    @game_over = Gosu::Font.new(32)
+    @game_over.draw("Game Over", 10, 200, 3)
+    @game_over.draw("Final Score:", 10, 250, 3)
+    @game_over.draw("#{@score}", 10, 300, 3)
+  end
+
+  def collision?(entity1, entity2)
+    if entity1 && entity2
+      distance = Gosu.distance(entity1.x, entity1.y, entity2.x, entity2.y)
+      distance < entity2.radius + entity1.radius
     end
   end
 end
