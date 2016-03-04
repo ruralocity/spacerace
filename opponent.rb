377b789cfc93899916ec2780ffca10a6dfac317e
Aaron Sumner <asumner@mac.com>
2016-03-04 04:04:05 -0600

    Make opponent speed variable

--- opponent.rb
+++ opponent.rb
@@ -1,5 +1,5 @@
 class Opponent
-  SPEED = 5
+  BASE_SPEED = 5
   attr_reader :x, :y, :radius
   attr_accessor :passed
 
@@ -7,12 +7,13 @@ class Opponent
     @radius = 20
     @x = rand(window.width - 2 * @radius) + @radius
     @y = 0
+    @speed = BASE_SPEED + rand(0..7)
     @image = Gosu::Image.new("images/ship.png")
     @passed = false
   end
 
   def move
-    @y += SPEED
+    @y += @speed
   end
 
   def draw
